f57feda25e2ae209ea1b20a1a785ad1e7d73ee6f
tiniakov <tiniakov79@gmail.com>
2025-05-04 16:51:34 +0300

    fix preflight request

--- api/src/main/java/practicum/sprint8/bionicpro_api/configuration/WebSecurityConfiguration.java
+++ api/src/main/java/practicum/sprint8/bionicpro_api/configuration/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package practicum.sprint8.bionicpro_api.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ public class WebSecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/main").permitAll()
                         .requestMatchers("/report").hasRole("prothetic_user")
                 );
